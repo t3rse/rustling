8c416beeebd759d2be7c48b1d15dc8d5efb86574
t3rse <david.seruyange@gmail.com>
2020-11-15 01:49:44 -0600

    WIP

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,9 +5,9 @@
 // I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = 0..1000;
 
-    if a.len() >= 100 {
+    if (a.len() >= 100) {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
